6f569bedcfba35f1e8522a1efff18f2002a18ccc
sVlast <sandro.vlastelica@gmail.com>
2023-08-09 22:35:52 +0200

    Updated home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,66 +1,49 @@
 import React from 'react';
-import { Container, Row } from 'react-bootstrap';
+import { Container, Row, Stack } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { css } from '@emotion/css';
 
 export const HomePage = (): JSX.Element => {
   return (
     <Container>
-      <Row className="justify-content-center">
-        <h1 className="text-center">Home page</h1>
-        {/* <Stack
-          direction="horizontal"
-          className={`${stackStyle} justify-content-center py-3 mx-auto bg-light`}
-          gap={3}
-        >
-          <Button>Home</Button>
-          <Button>List</Button>
-          <Button>Image Galery</Button>
-          <Button>Contact Form</Button>
-          <Button>Settings</Button>
-        </Stack> */}
+      <Row className="justify-content-center mt-3">
+        <Stack direction="horizontal" className={'justify-content-center'}>
+            <h1>Home page</h1>
+        </Stack>
       </Row>
       <Row className="col-8">
         <div>Author: Sandro Vlastelica</div>
         <div>App made for: Interaktivno programiranje na Web-u </div>
-        <div>Features: </div>
         <div>
-          <b>List:</b>- stranica povlaci objekte s vanjskog api-ja, moguce
-          povlacenje vise vrsta podataka ovisno odabranom svojstvu
+          <b>List:</b>
         </div>
-        <div> - svaki objekt sadrzi listu koja expand-at</div>
+        <ul>
+          <li>stranica dohvaća json response s api-a i sprema ih kao objekte, moguće dohvaćanje više vrsta podataka ovisno odabranom svojstvu</li>
+          <li>svaki objekt sadrzi listu koju je moguće proširiti</li>
+          <li>moguće je dodavanje podataka iz dohvaćene liste u zasebnu listu, moguće je brisanje podataka iz te liste</li>
+        </ul>
         <div>
-          {' '}
-          - moguce je dodavanje podataka iz dohvacene liste u zasebnu listu,
-          moguce je brisanje podataka iz te liste
+          <b>Image Galery:</b>
         </div>
+        <ul>
+          <li>sastoji se od slideshow-a, mogućnost pokretanja i zaustavljanja</li>
+        </ul>
         <div>
-          <b>Image Galery:</b>- sastoji se od slideshow-a, mogucnost pokretanja
-          i zaustavljanja
+          <b>Contact Form:</b>
         </div>
+        <ul>
+          <li>forma sadrzi inpute za username, email, subject, message</li>
+          <li>svi inputi moraju biti ispunjeni</li>
+          <li>username ima provjeru za minimalni broj znakova</li>
+          <li>email mora biti pravilnog oblika, npr: <em>ime.prezime@host.com</em></li>
+          <li>ako se ne zadovolje uvjeti pojedini inputi dobivaju crveni obrub i ispisuje se poruka što je neispravno</li>
+          <li>ako je forma pravilno ispunjena, submit prikazuje unešene podatke u konzoli</li>
+        </ul>
         <div>
-          <b>Contact Form:</b>- forma sadrzi inpute za username, email, subject,
-          message
+          <b>Settings:</b>
         </div>
-        <div> - svi inputi moraju biti ispunjeni</div>
-        <div> - username ima provjeru za minimalni broj znakova</div>
-        <div>
-          {' '}
-          - email mora biti pravilnog npr: <em>ime.prezime@host.com</em>
-        </div>
-        <div>
-          {' '}
-          - ako se ne zadovolje uvjeti zasebni inputi dobivaju crveni obrub i
-          ispisuje se poruka sto je neispravno ispod{' '}
-        </div>
-        <div>
-          - ako je forma pravilno ispunjena, submit prikazuje unešene podatke u
-          konzoli
-        </div>
-        <div>
-          <b>Settings:</b> - sadrzi postavke za veličinu fonta i temu stranice,
-          postavke se spremaju u local storage
-        </div>
+        <ul>
+          <li>sadrži postavke za veličinu fonta i temu stranice, postavke se spremaju u local storage</li>
+        </ul>
       </Row>
     </Container>
   );
